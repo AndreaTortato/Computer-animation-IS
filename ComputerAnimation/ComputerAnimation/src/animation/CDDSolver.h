b5b6360638383b5af492334f8df7b7478e3cd120
AndreaTortato <atortato@tradeheader.com>
2024-02-16 18:17:52 +0100

    -

---
+++ ComputerAnimation/ComputerAnimation/src/animation/CDDSolver.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+#include "../math/transform.h"
+#include "IKSolver.h"
+
+class CCDSolver : public IKSolver {
+protected:
+	Transform auxParent; // in global space
+public:
+	bool solve(const Transform& target);
+};
